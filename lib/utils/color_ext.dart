b60b3e4fcc39845a89596dad46ed22c23682924e
Đỗ Dương Tâm Đăng <72535123+DangDDT@users.noreply.github.com>
2024-04-17 11:18:38 +0700

    style: Sửa lỗi chính tả

--- lib/utils/color_ext.dart
+++ lib/utils/color_ext.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 extension ColorX on Color {
-  Color get contractColor =>
+  Color get contrastColor =>
       computeLuminance() > 0.5 ? Colors.black : Colors.white;
 }
